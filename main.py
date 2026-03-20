74f26690caf33553bea00dd38e0f95dbdacb3bf3
Margarida Cunha <margaridapassoscunha@gmail.com>
2026-03-20 19:04:29 +0000

    Fix bugs in main.py

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ import sys
 
 if __name__ == "__main__":
     paths = download_all_datasets()
-    print("Downloaded:")
     for k, v in paths.items():
         print(f"- {k}: {v}")
-
-if __name__ == "__main__":
     subprocess.run(
         [sys.executable, "-m", "streamlit", "run", "app/streamlit_app.py"],
         check=True,
